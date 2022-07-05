e6663236af2d27efefd65461c4098bde3e3d8f65
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-06 02:25:33 +0800

    debug

--- preprocess/merge_preprocess/demo_pare_result_merge.py
+++ preprocess/merge_preprocess/demo_pare_result_merge.py
@@ -344,6 +344,7 @@ def main2(**args):
     result_path = args.pop('pare_result')
     pare_result = joblib.load(result_path)
     print('nframes:',nframes)
+    print('img_folder:',img_folder)
     print('nframes2:',len(glob.glob(os.path.join(img_folder, '*jpg'))))
     test_path='/root/code/mover/preprocess/input_data/Color_flip/imgs'
     print('nframes3:',len(glob.glob(os.path.join(test_path, '*jpg'))))
--- preprocess/merge_preprocess/merge.py
+++ preprocess/merge_preprocess/merge.py
@@ -58,6 +58,23 @@ if __name__ == "__main__":
     model_folder = 'data/body_models'
     vposer_folder = '../../smplifyx-file/vposer_v1_0'
     segm_fn_path='../../smplifyx-file/smplx_parts_segm.pkl'
+    '''
+    #    --config ./cfg_files/fit_smpl.yaml \
+    #    --export_mesh True \
+    #    --save_new_json True \
+    #    --json_folder  ${JSON_FOLDER} \
+    #    --data_folder ${DATA_FOLDER} \
+    #    --output_folder ${OUTPUT_FOLDER} \
+    #    --pare_result ${pare_result} \
+    #    --cam_dir ${cam_dir} \
+    #    --visualize="False" \
+    #    --model_folder ${MODEL_FOLDER} \
+    #    --vposer_ckpt ${VPOSER_FOLDER} \
+    #    --part_segm_fn /root/code/mover/smplifyx-file/smplx_parts_segm.pkl \
+    #    --gender male \
+    #    --check_inverse_feet="False" \
+    '''
+
     parser2.set_defaults(config='cfg_files/fit_smpl.yaml', export_mesh=True, save_new_json=True,
                          json_folder=f'{json_folder}', data_folder=f'{image_dir}', output_folder=f'{output_folder}',
                          pare_result=f'{pare_result}', cam_dir=f'{cam_dir}', visualize=False,
