5d3bbd3db315036c63232bf3b6f6da7620f3c441
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-05 18:02:14 +0800

    change op filter path

--- preprocess/merge_preprocess/merge.py
+++ preprocess/merge_preprocess/merge.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('footskate_reducer')
+sys.path.append('footskate_reducer/ground_detector')
 from footskate_reducer.ground_detector.op_filter_json_merge import main0
 from demo_merge import main1
 from demo_pare_result_merge import main2
