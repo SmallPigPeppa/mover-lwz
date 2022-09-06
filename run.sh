301d3819bc857838455948e7a8636c5d97726dc6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-06 22:36:14 +0800

    jpg to png

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ conda activate pare-env
 cd preprocess
 python merge.py \
         --video_name Color_flip \
-        --out_dir /root/code/mover-lwz/out-data \
-        --input_dir /root/code/mover-lwz/input-data
+        --out_dir /share/wenzhuoliu/code/mover-lwz/out-data \
+        --input_dir /share/wenzhuoliu/code/mover-lwz/input-data
