88a288075b1fa895c13cf595a4cb1a739c733976
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-10-11 19:54:25 +0800

    clean unused files

--- modify_hdf5_2.py
+++ modify_hdf5_2.py
@@ -7,14 +7,14 @@ info_npz_name = "samples_clean_gta/FPS-5/2020-06-11-10-06-48/info_frames.npz"
 # info_npz = np.load(open(info_npz_name, 'rb'))
 info_npz = np.load(info_npz_name)
 print(info_npz['joints_3d_world'].shape)
+joints = np.einsum('zxy->xyz', info_npz['joints_3d_world'])
 with h5py.File(gta_name, "r+") as f:
     print("Keys: %s" % f.keys())
     del f['skeleton_joints']
     print("Keys: %s" % f.keys())
-    f.create_dataset('skeleton_joints', data=info_npz['joints_3d_world'])
+    f.create_dataset('skeleton_joints', data=joints)
     print(f['skeleton_joints'])
     print("Keys: %s" % f.keys())
-
 
 # with h5py.File(gta_name, "r") as f:
 #     print("Keys: %s" % f.keys())
