bbe8f9427acc65fdbe4d0c6722c7eacd670bd394
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-10-11 19:44:29 +0800

    clean unused files

--- modify_hdf5_2.py
+++ modify_hdf5_2.py
@@ -1,13 +1,13 @@
 import h5py
 import numpy as np
 
-temp_name = "samples_clean/3_3_78_Female2_0.hdf5"
+# temp_name = "samples_clean/3_3_78_Female2_0.hdf5"
 gta_name = "samples_clean_gta/FPS-5-clean/FPS-5-2020-06-11-10-06-48.hdf5"
 info_npz_name = "samples_clean_gta/FPS-5/2020-06-11-10-06-48/info_frames.npz"
 # info_npz = np.load(open(info_npz_name, 'rb'))
 info_npz = np.load(info_npz_name)
 print(info_npz['joints_3d_world'].shape)
-with h5py.File(temp_name, "r+") as f:
+with h5py.File(gta_name, "r+") as f:
     print("Keys: %s" % f.keys())
     del f['skeleton_joints']
     print("Keys: %s" % f.keys())
