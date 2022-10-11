041fbcd17cd1e823b4b98ce8640f845250b7d977
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-10-11 23:06:37 +0800

    clean unused files

--- body_models/video_smplifyx/fit_video.py
+++ body_models/video_smplifyx/fit_video.py
@@ -1183,7 +1183,7 @@ def fit_multi_view(img,
                         expression = expression,
                         )
 
-            results[min_idx]['result']['keypoints_3d'] = model_output.joints.detach().cpu().numpy()[:, :25, :]
+            # results[min_idx]['result']['keypoints_3d'] = model_output.joints.detach().cpu().numpy()[:, :25, :]
             results[min_idx]['result']['body_pose'] = body_pose.detach().cpu().numpy()
             results[min_idx]['result']['pose_embedding'] = pose_embedding.detach().cpu().numpy()
             results[min_idx]['result']['gender'] = kwargs['gender']
