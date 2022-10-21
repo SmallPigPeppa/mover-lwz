42e4e4c59d35ac89355b187c0ed5d8d2d6955278
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-10-21 12:46:44 +0800

    clean unused files

--- gta_im2smplx_hdf5_test2.py
+++ gta_im2smplx_hdf5_test2.py
@@ -129,6 +129,7 @@ hdf5_name = "samples_clean_gta/FPS-5-clean-debug/FPS-5-2020-06-11-10-06-48.hdf5"
 # joints = info_npz['joints_3d_camera']
 # img_list=[24]
 img_list=list(range(408,955))
+img_list=list(range(0,955))
 with h5py.File(hdf5_name, "r+") as f:
     print("Keys: %s" % f.keys())
     del f['skeleton_joints']
