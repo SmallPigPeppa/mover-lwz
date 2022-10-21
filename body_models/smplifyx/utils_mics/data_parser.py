235d801b429dd9bd4c58aa4bd76cfa249764aa80
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-10-21 12:47:54 +0800

    clean unused files

--- body_models/smplifyx/utils_mics/data_parser.py
+++ body_models/smplifyx/utils_mics/data_parser.py
@@ -409,7 +409,8 @@ class OpenPose_Pose2Room(Dataset):
         img_fn_tmp = glob.glob('/share/wenzhuoliu/code/mover-lwz/input-data/gta-test/images/*.png')
         img_fn=[]
         for i in img_fn_tmp:
-            if int(i.split('/')[-1].split('.')[0]) in range(408,955):
+            # if int(i.split('/')[-1].split('.')[0]) in range(408,955):
+            if int(i.split('/')[-1].split('.')[0]) in range(0, 955):
                 img_fn.append(i)
 
 
