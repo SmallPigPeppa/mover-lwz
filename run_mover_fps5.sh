c1a91cedd425cad404ad2b8a8ee50ae7487e2830
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-19 20:24:05 +0800

    clean unused files

--- run_mover_fps5.sh
+++ run_mover_fps5.sh
@@ -19,23 +19,23 @@ echo ${OUTPUT_FOLDER}
 #conda activate mover
 #cd /share/wenzhuoliu/code/mover-lwz-fit3d-smpl
 python main.py \
-    --single "False" \
-    --dataset "Pose2Room" \
+    --single False \
+    --dataset Pose2Room \
     --config ${CONFIG_FILE} \
     --img_list ${img_list} \
     --batch_size ${batch_size} \
     --data_folder ${DATA_FOLDER} \
     --output_folder ${OUTPUT_FOLDER} \
-    --visualize="False" \
-    --save_meshes="False" \
+    --visualize False \
+    --save_meshes False \
     --model_folder ${MODEL_FOLDER} \
-    --model_type 'smpl' \
-    --pre_load="False" \
-    --pre_load_pare_pose="False" \
+    --model_type smpl \
+    --pre_load False \
+    --pre_load_pare_pose False \
     --vposer_ckpt ${VPOSER_FOLDER} \
     --part_segm_fn ${PART_SEGM_FN} \
-    --camera_type "user" \
-    --gender 'neutral' \
+    --camera_type user \
+    --gender neutral \
     --calib_path ${CALIBRATION_FOLDER} \
     --start_opt_stage 3 \
     --end_opt_stage 4
