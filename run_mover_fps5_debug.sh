637884e7f0d0d279b58333974bcb076cadd4da58
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-19 20:18:51 +0800

    clean unused files

--- run_mover_fps5_debug.sh
+++ run_mover_fps5_debug.sh
@@ -3,10 +3,9 @@ batch_size=1
 img_list=-1
 #PROJECT_PATH="/mnt/mmtech01/usr/liuwenzhuo/code/mover-lwz-fit3d-smpl-all"
 PROJECT_PATH="/share/wenzhuoliu/code/mover-lwz-fit3d-smpl-all"
-REC_IDX="2020-06-09-16-09-56"
 
 
-DATA_FOLDER="${PROJECT_PATH}/mover-input/FPS-5/${REC_IDX}"
+DATA_FOLDER="${PROJECT_PATH}/mover-input/FPS-5/"
 OUTPUT_FOLDER="${PROJECT_PATH}/mover-out"
 CALIBRATION_FOLDER="${PROJECT_PATH}/smplifyx_cam/"
 CONFIG_FILE="${PROJECT_PATH}/body_models/cfg_files/fit_smpl_3D.yaml"
@@ -20,23 +19,23 @@ echo ${OUTPUT_FOLDER}
 #conda activate mover
 #cd /share/wenzhuoliu/code/mover-lwz-fit3d-smpl
 python main.py \
-    --single "False" \
-    --dataset "Pose2Room" \
+    --single False \
+    --dataset Pose2Room \
     --config ${CONFIG_FILE} \
     --img_list ${img_list} \
     --batch_size ${batch_size} \
     --data_folder ${DATA_FOLDER} \
     --output_folder ${OUTPUT_FOLDER} \
-    --visualize="False" \
-    --save_meshes=False \
+    --visualize False \
+    --save_meshes False \
     --model_folder ${MODEL_FOLDER} \
-    --model_type 'smpl' \
-    --pre_load="False" \
-    --pre_load_pare_pose="False" \
+    --model_type smpl \
+    --pre_load False \
+    --pre_load_pare_pose False \
     --vposer_ckpt ${VPOSER_FOLDER} \
     --part_segm_fn ${PART_SEGM_FN} \
-    --camera_type "user" \
-    --gender 'neutral' \
+    --camera_type user \
+    --gender neutral \
     --calib_path ${CALIBRATION_FOLDER} \
     --start_opt_stage 3 \
     --end_opt_stage 4
