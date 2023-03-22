e22347e6d03e7835f8b7d9dc643436cfd800dbe3
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-22 15:43:02 +0800

    clean unused files

---
+++ run_cp_mover_out_fps30.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dirA="/mnt/mmtech01/usr/liuwenzhuo/code/mover-lwz-fit3d-smpl-all/mover-out/FPS-30"
+dirB="/mnt/mmtech01/dataset/vision_text_pretrain/gta-im/FPS-30"
+
+for subdir in "$dirA"/*; do
+    if [ -d "$subdir" ]; then
+        subdir_name=$(basename "$subdir")
+        src_file="$subdir/results/001_all.pkl"
+        dest_dir="$dirB/$subdir_name"
+        dest_file="$dest_dir/info_smpl.pkl"
+        if [ -e "$src_file" ] && [ -d "$dest_dir" ]; then
+            cp "$src_file" "$dest_file"
+        fi
+    fi
+done
