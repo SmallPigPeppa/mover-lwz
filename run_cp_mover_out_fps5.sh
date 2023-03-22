989f50ab308fab51d9df4ff055e54009be3ca10f
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-22 15:37:14 +0800

    clean unused files

--- run_cp_mover_out_fps5.sh
+++ run_cp_mover_out_fps5.sh
@@ -8,8 +8,9 @@ for subdir in "$dirA"/*; do
         subdir_name=$(basename "$subdir")
         src_file="$subdir/results/001_all.pkl"
         dest_dir="$dirB/$subdir_name"
+        dest_file="$dest_dir/info_smpl.pkl"
         if [ -e "$src_file" ] && [ -d "$dest_dir" ]; then
-            cp "$src_file" "$dest_dir"
+            cp "$src_file" "$dest_file"
         fi
     fi
 done
